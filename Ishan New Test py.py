637b09740675ab4a5fb60de69499ed6162596a22
IshanChakraborty96 <91935558+IshanChakraborty96@users.noreply.github.com>
2021-10-07 18:09:10 +0530

    Add files via upload

--- Ishan New Test py.py
+++ Ishan New Test py.py
@@ -2658,6 +2658,101 @@ while current_choice != "0":
     current_choice = input("> ")
 
 
+# # contents
+
+# In[2]:
+
+
+pantry = {
+    "chicken": 500,
+    "lemon": 2,
+    "cumin": 24,
+    "paprika": 18,
+    "chilli powder": 7,
+    "yogurt": 300,
+    "oil": 450,
+    "onion": 5,
+    "garlic": 9,
+    "ginger": 2,
+    "tomato puree": 125,
+    "almonds": 75,
+    "rice": 500,
+    "coriander": 20,
+    "lime": 3,
+    "pepper": 8,
+    "egg": 6,
+    "pizza": 2,
+    "spam": 1,
+}
+
+recipes = {
+    "Butter chicken": [
+        "chicken",
+        "lemon",
+        "cumin",
+        "paprika",
+        "chilli powder",
+        "yogurt",
+        "oil",
+        "onion",
+        "garlic",
+        "ginger",
+        "tomato puree",
+        "almonds",
+        "rice",
+        "coriander",
+        "lime",
+    ],
+    "Chicken and chips": [
+        "chicken",
+        "potatoes",
+        "salt",
+        "malt vinegar",
+    ],
+    "Pizza": [
+        "pizza",
+    ],
+    "Egg sandwich": [
+        "egg",
+        "bread",
+        "butter",
+    ],
+    "Beans on toast": [
+        "beans",
+        "bread",
+    ],
+    "Spam a la tin": [
+        "spam",
+        "tin opener",
+        "spoon",
+    ],
+}
+
+
+# # dict_methods
+
+# In[5]:
+
+
+d = {
+    0: "zero",
+    1: "one",
+    2: "two",
+    3: "three",
+    4: "four",
+    5: "five",
+    6: "six",
+    7: "seven",
+    8: "eight",
+    9: "nine",
+}
+
+pantry_items = ['chicken', 'spam', 'egg', 'bread', 'lemon']
+
+new_dict = dict.fromkeys(pantry_items)
+print(new_dict)
+
+
 # In[ ]:
 
 
